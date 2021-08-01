be7f84a7f799f3405d96c73dd8213e40fe3b30ab
Kishore Saha <sahakishore4@gmail.com>
2021-08-01 22:21:56 +0530

    few change

--- components/Layout.js
+++ components/Layout.js
@@ -2,14 +2,21 @@ import Link from 'next/link'
 
 export default function Layout({ children }) {
   return (
+    <>
+      <div className="container">
+      <div className="myname">
+        <h6>Developed by: Kishore Saha</h6>
+        <span>Frontend: Next.js   </span> <br></br>
+        <span>Backend: Contentful api</span>
+    </div>
+
+      </div>
     <div className="layout">
       <header>
         <Link href="/">
           <a>
             <h1>
-              <div>
-                <h3>Developed by Kishore Saha</h3>
-              </div>
+              
               <span>Just Add</span>
               <span>Marmite</span>
             </h1>
@@ -26,5 +33,6 @@ export default function Layout({ children }) {
         <p>Copyright 2021 Just Add Marmite </p>
       </footer>
     </div>
+    </>
   )
 }
